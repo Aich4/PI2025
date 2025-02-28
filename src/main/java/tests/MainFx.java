a3c95f421351263e7809598ccfc966da4a4ec326
natej-ghodbane <natej.ghodbane@esprit.tn>
2025-02-28 15:22:00 +0100

    ..

--- src/main/java/tests/MainFx.java
+++ src/main/java/tests/MainFx.java
@@ -14,7 +14,7 @@ public class MainFx extends Application {
     @Override
 
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ListDestinationBack.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/FrontOffice.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
         stage.setScene(scene);
