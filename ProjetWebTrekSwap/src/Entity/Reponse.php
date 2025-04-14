b7c53bb15cbfae5a166b3408364940fbfcc40f1f
AhmedBouhlel69 <bouhlel.ahmed@esprit.tn>
2025-04-14 14:35:37 +0100

    crud et assert

--- ProjetWebTrekSwap/src/Entity/Reponse.php
+++ ProjetWebTrekSwap/src/Entity/Reponse.php
@@ -4,66 +4,70 @@ namespace App\Entity;
 
 use App\Repository\ReponseRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ReponseRepository::class)]
 class Reponse
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    
-    #[ORM\Column]
-    private ?int $id_rep = null;
+    #[ORM\Column(name: 'id_rep')]
+    private ?int $id = null;
 
-    #[ORM\Column]
-    private ?int $id_rec = null;
+    #[ORM\ManyToOne(targetEntity: Reclamation::class, inversedBy: 'reponses')]
+    #[ORM\JoinColumn(name: 'id_rec', referencedColumnName: 'id_rec')]
+    #[Assert\NotNull(message: "La réclamation ne peut pas être vide")]
+    private ?Reclamation $reclamation = null;
 
-    public function getIdRec(): ?int
+    #[ORM\Column(name: 'date_rep')]
+    #[Assert\NotNull(message: "La date ne peut pas être vide")]
+    private ?\DateTimeImmutable $dateRep = null;
+
+    #[ORM\Column(name: 'contenu_rep', length: 255)]
+    #[Assert\NotBlank(message: "Le contenu ne peut pas être vide")]
+    #[Assert\Length(
+        min: 10,
+        max: 255,
+        minMessage: "Le contenu doit contenir au moins {{ limit }} caractères",
+        maxMessage: "Le contenu ne peut pas dépasser {{ limit }} caractères"
+    )]
+    private ?string $contenu = null;
+
+    public function getId(): ?int
     {
-        return $this->id_rec;
+        return $this->id;
     }
-    public function setIdRec(int $id_rec): static
-    {
-        $this->id_rec = $id_rec;
 
+    public function getReclamation(): ?Reclamation
+    {
+        return $this->reclamation;
+    }
+
+    public function setReclamation(?Reclamation $reclamation): static
+    {
+        $this->reclamation = $reclamation;
         return $this;
     }
-
-
-    #[ORM\Column]
-    private ?\DateTimeImmutable $date_rep = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $contenu_rep = null;
-
-    public function getId_rep(): ?int
-    {
-        return $this->id_rep;
-    }
-    
 
     public function getDateRep(): ?\DateTimeImmutable
     {
-        return $this->date_rep;
+        return $this->dateRep;
     }
 
-    public function setDateRep(\DateTimeImmutable $date_rep): static
+    public function setDateRep(\DateTimeImmutable $dateRep): static
     {
-        $this->date_rep = $date_rep;
-
+        $this->dateRep = $dateRep;
         return $this;
     }
 
-    public function getContenuRep(): ?string
+    public function getContenu(): ?string
     {
-        return $this->contenu_rep;
+        return $this->contenu;
     }
 
-    public function setContenuRep(string $contenu_rep): static
+    public function setContenu(string $contenu): static
     {
-        $this->contenu_rep = $contenu_rep;
-
+        $this->contenu = $contenu;
         return $this;
     }
-
-    
 }
